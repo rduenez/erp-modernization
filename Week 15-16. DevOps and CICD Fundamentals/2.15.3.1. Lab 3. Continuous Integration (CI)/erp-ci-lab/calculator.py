f4cb54312e2e116a35a9a47526690bbce4fc5b3c
Ronald Duenez <ronald.du.or@gmail.com>
2026-06-17 20:46:14 -0600

    Fixing: test_tax_calculator.py::test_calculate_total_with_iva FAILED             [100%]

--- Week 15-16. DevOps and CICD Fundamentals/2.15.3.1. Lab 3. Continuous Integration (CI)/erp-ci-lab/calculator.py
+++ Week 15-16. DevOps and CICD Fundamentals/2.15.3.1. Lab 3. Continuous Integration (CI)/erp-ci-lab/calculator.py
@@ -3,6 +3,6 @@ def calculate_total_with_iva(subtotal):
     Calculates the total price including 16% IVA.
     """
     # Intentional Bug: Using 0.15 instead of 0.16
-    iva_rate = 0.15 
+    iva_rate = 0.16 
     iva_amount = subtotal * iva_rate
     return subtotal + iva_amount
